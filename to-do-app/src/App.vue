e04963c06bfcbc6498e7490ad3d21ccf83327779
sanjayjatti <jattisanjay.r@gmail.com>
2025-04-14 19:12:53 +0530

    stored data in localStorage

--- to-do-app/src/App.vue
+++ to-do-app/src/App.vue
@@ -1,25 +1,38 @@
 <script setup>
 import TodoList from "./components/TodoList.vue";
 import AddTodo from "./components/AddTodo.vue";
-import { ref, onMounted, computed} from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import axios from "axios";
 
 const todos = ref([]);
-const filter = ref('all')
+const filter = ref("all");
+
+watch(
+  todos,
+  (newTodos) => {
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  },
+  { deep: true }
+);
 
 const fetchTodos = async () => {
-  try {
-    const res = await axios.get(
-      "https://jsonplaceholder.typicode.com/todos?_limit=10"
-    );
-    todos.value = res.data;
-  } catch (err) {
-    console.log(err);
+  const storedTodos = localStorage.getItem("todos");
+  if (storedTodos) {
+    todos.value = JSON.parse(storedTodos);
+  } else {
+    try {
+      const res = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+      );
+      todos.value = res.data;
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 const addTodo = (todo) => {
   todos.value.unshift(todo);
-    return filter.value = 'all'
+  filter.value = "all";
 };
 const toggleTodo = (id) => {
   const todo = todos.value.find((item) => item.id === id);
@@ -35,22 +48,24 @@ const handleEditTodo = ({ id, title }) => {
 };
 onMounted(fetchTodos);
 
-const filteredTodos = computed(()=>{
-  if(filter.value === 'completed') return todos.value.filter( item => item.completed);
-  if(filter.value === 'incomplete') return todos.value.filter( item => !item.completed);
+const filteredTodos = computed(() => {
+  if (filter.value === "completed")
+    return todos.value.filter((item) => item.completed);
+  if (filter.value === "incomplete")
+    return todos.value.filter((item) => !item.completed);
   return todos.value;
-})
+});
 
 const showCompletedOnly = () => {
-  return filter.value = 'completed'
+  return (filter.value = "completed");
 };
 const showIncompletedOnly = () => {
-  return filter.value = 'incomplete'
+  return (filter.value = "incomplete");
 };
 
-const showAll=()=>{
-  return filter.value = 'all'
-}
+const showAll = () => {
+  return (filter.value = "all");
+};
 </script>
 
 <template>
@@ -59,7 +74,7 @@ const showAll=()=>{
   <div class="m-t-16">
     <button @click="showAll">All</button>
     <button @click="showCompletedOnly">Completed</button>
-    <button @click="showIncompletedOnly">InComplete</button>
+    <button @click="showIncompletedOnly">Incomplete</button>
   </div>
   <TodoList
     :todos="filteredTodos"
